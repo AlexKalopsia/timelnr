109f3ad6362c960afc96660c04f2d6722d1d31d1
Alex Camilleri <camilleri.alex@gmail.com>
2020-07-26 21:20:16 +0200

    Moved labels data to db

--- timelnr/models.py
+++ timelnr/models.py
@@ -9,3 +9,15 @@ class Label():
 
     def __str__(self):
         return 'Label(slug:{}, name:{}, color:{})'.format(self.slug, self.name, self.color)
+
+
+class Entry():
+    def __init__(self, id: int, content: str, year: int, label: str, imageURL: str):
+        self.id = id
+        self.content = content
+        self.year = year
+        self.label = label
+        self.imageURL = imageURL
+
+    def __str__(self):
+        return 'Entry(id:{}, content:{}, year:{}, label:{}, imageURL:{})'.format(self.id, self.content, self.year, self.label, self.imageURL)
--- timelnr/routes.py
+++ timelnr/routes.py
@@ -1,6 +1,6 @@
 from flask import redirect, render_template, url_for
 from timelnr import app, db
-from timelnr.config import langs, labels, label_colors
+from timelnr.config import langs
 
 
 @app.route("/timeline")
@@ -16,10 +16,45 @@ def root():
 @app.route("/<string:curr_lang>")
 def home(curr_lang):
     with db.connect() as connection:
+
+        labels = list(connection.execute(
+            "SELECT `mgt_labels`.* FROM `mgt_labels`;"))
+
+        list_labels = [
+            {
+                'id': id,
+                'slug': slug,
+                'name': name,
+                'color': color
+            }
+            for id, slug, name, color in labels
+        ]
+
         result = connection.execute(
             "SELECT `mgt_entries`.* FROM `mgt_entries`;")
+
+        # Query joins entry and label if the color matches the label slug
+        # result = connection.execute(
+        #    "SELECT * FROM mgt_entries entry LEFT JOIN mgt_labels label ON entry.mgtColor = label.labelSlug ORDER BY mgtID")
+
         entries = []
         for row in result:
+
+            label = next(
+                (
+                    {'slug': l[1],
+                     'name': l[2],
+                     'color': l[3]}
+                    for l in labels
+                    if l[1] == row["mgtColor"]
+                ),
+                {
+                    'slug': '',
+                    'name': '',
+                    'color': ''
+                }
+            )
+
             entry = {
                 'id': row['mgtID'],
                 'year': row['mgtYear'],
@@ -27,11 +62,7 @@ def home(curr_lang):
                 'game': row['mgtGame'],
                 'source': row['mgtSource'],
                 'image': row['mgtImg'],
-                'label': {
-                    'slug': row['mgtColor'],
-                    'color': label_colors.get(row['mgtColor']),
-                    'name': row['mgtGame']
-                }
+                'label': label
             }
             entries.append(entry)
-    return render_template('timeline.html', entries=entries, curr_lang=curr_lang, langs=langs, labels=labels)
+    return render_template('timeline.html', entries=entries, curr_lang=curr_lang, langs=langs, labels=list_labels)
