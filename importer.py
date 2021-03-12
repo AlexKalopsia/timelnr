571634fc90929b76433eba29e0668762cccd5af5
AlexKalopsia <alex@kalopsiagames.com>
2021-03-12 21:17:40 +0100

    Improved importer verbosity

--- importer.py
+++ importer.py
@@ -24,18 +24,22 @@ def getDataFromSheet():
     # created automatically when the authorization flow completes for the first
     # time.
     if os.path.exists('token.pickle'):
+        print('Loading credentials from file...')
         with open('token.pickle', 'rb') as token:
             creds = pickle.load(token)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
+            print('Refreshing access token...')
             creds.refresh(Request())
         else:
+            print('Fetching new tokens...')
             flow = InstalledAppFlow.from_client_secrets_file(
                 'credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
         with open('token.pickle', 'wb') as token:
+            print('Saving credentials for future use...')
             pickle.dump(creds, token)
 
     service = build('sheets', 'v4', credentials=creds)
@@ -60,9 +64,11 @@ def createTable():
 
     # Wipe table
     with db.connect() as connection:
+        print('Wiping existing table...')
         connection.execute('DROP TABLE IF EXISTS `' + config.DB_TABLE + '`')
 
     # Prepare table
+    print('Preparing table...')
     timeline = Table(config.DB_TABLE, metadata,
                      Column('mgtID', Integer, primary_key=True),
                      Column('mgtYear', String(4)),
@@ -75,6 +81,7 @@ def createTable():
                      )
     for lang in langs:
         timeline.append_column(Column('mgtEvent_' + lang, String(900)))
+    print('Creating table...')
     metadata.create_all(db)
     return timeline
 
@@ -83,6 +90,7 @@ def populateTable(timeline):
 
     values = getDataFromSheet()
     langCodes = values[1][7:]
+    print('Populating table...')
     with db.connect() as connection:
         for value in values[2:]:
             connection.execute(timeline.insert().values(value))
@@ -91,6 +99,7 @@ def populateTable(timeline):
 def main():
     tl = createTable()
     populateTable(tl)
+    print('Import successful')
 
 
 if __name__ == '__main__':
